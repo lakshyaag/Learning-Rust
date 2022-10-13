c1712e827e9d0ce78b4bfc61be2e65f100a13a7b
Lakshya Agarwal <lakshyajannu@gmail.com>
2022-10-13 10:21:12 +0530

    Initial commit

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,21 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn display_first_name() {
+    println!("Lakshya");
+}
+
+fn display_last_name() {
+    println!("Agarwal");
+}
+fn main() {
+    display_first_name();
+    display_last_name();
+}
